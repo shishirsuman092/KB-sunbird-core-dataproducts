880238e8645b3e9bed17111ece0fffdd1c017631
FsherinP <fathimasherin362@gmail.com>
2024-10-01 16:12:03 +0530

    fixes - CBA

--- batch-models/src/main/scala/org/ekstep/analytics/dashboard/report/cba/CourseBasedAssessmentModel.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/dashboard/report/cba/CourseBasedAssessmentModel.scala
@@ -55,7 +55,7 @@ object CourseBasedAssessmentModel extends AbsDashboardModel {
 
     val finalDF = userAssessChildDataLatestDF
       .withColumn("userAssessmentDuration", unix_timestamp(col("assessEndTimestamp")) - unix_timestamp(col("assessStartTimestamp")))
-      .withColumn("Pass", expr("CASE WHEN assessPass == 1 THEN 'Yes' ELSE 'No' END"))
+      .withColumn("Pass", when(col("assessPass") === 1, "Yes").otherwise("No"))
       .durationFormat("assessExpectedDuration", "totalAssessmentDuration")
       .withColumn("assessPercentage", when(col("assessPassPercentage").isNotNull, col("assessPassPercentage"))
         .otherwise(lit("Need to pass in all sections")))
@@ -68,15 +68,16 @@ object CourseBasedAssessmentModel extends AbsDashboardModel {
       .withColumn("course_id", when(col("assessCategory") === "Standalone Assessment", lit(""))
         .otherwise(col("assessID")))
       .withColumn("Tags", concat_ws(", ", col("additionalProperties.tag")))
+      .cache()
 
     val fullReportDFNew = finalDF
       .select(
-        col("userID").alias("User ID"),
+        col("userID"),
+        col("assessChildID").alias("assessment_id"),
         col("assessID"),
         col("assessOrgID"),
-        col("assessChildID"),
         col("userOrgID"),
-        col("fullName").alias("Full Name"),
+        col("fullName"),
         col("professionalDetails.designation").alias("Designation"),
         col("personalDetails.primaryEmail").alias("E mail"),
         col("personalDetails.mobile").alias("Phone Number"),
@@ -85,21 +86,21 @@ object CourseBasedAssessmentModel extends AbsDashboardModel {
         col("ministry_name").alias("Ministry"),
         col("dept_name").alias("Department"),
         col("userOrgName").alias("Organisation"),
-        col("assessChildName").alias("Assessment Name"),
-        col("assessment_type").alias("Assessment Type"),
-        col("assessOrgName").alias("Assessment/Content Provider"),
-        from_unixtime(col("assessLastPublishedOn").cast("long"), dateFormat).alias("Assessment Publish Date"),
-        col("assessment_course_name").alias("Course Name"),
-        col("course_id").alias("Course ID"),
-        col("totalAssessmentDuration").alias("Assessment Duration"),
-        from_unixtime(col("assessEndTime"), dateFormat).alias("Last Attempted Date"),
-        col("assessOverallResult").alias("Latest Percentage Achieved"),
-        col("assessPercentage").alias("Cut off Percentage"),
+        col("assessChildName").alias("assessment_name"),
+        col("assessment_type"),
+        col("assessOrgName").alias("assessment_content_provider"),
+        from_unixtime(col("assessLastPublishedOn").cast("long"), dateFormat).alias("assessment_publish_date"),
+        col("assessment_course_name").alias("course_name"),
+        col("course_id"),
+        col("totalAssessmentDuration").alias("assessment_duration"),
+        from_unixtime(col("assessEndTime"), dateFormat).alias("last_attempted_date"),
+        col("assessOverallResult").alias("latest_percentage_achieved"),
+        col("assessPercentage"),
         col("Pass"),
-        col("assessMaxQuestions").alias("Total Questions"),
-        col("assessIncorrect").alias("No.of Incorrect Responses"),
-        col("assessBlank").alias("Unattempted Questions"),
-        col("retakes").alias("No. of Retakes"),
+        col("assessMaxQuestions").alias("total_questions"),
+        col("assessIncorrect").alias("incorrect_count"),
+        col("assessBlank").alias("unattempted_questions"),
+        col("retakes"),
         col("assessEndTime"),
         col("userOrgID").alias("mdoid")
       ).repartition(1)
@@ -107,28 +108,26 @@ object CourseBasedAssessmentModel extends AbsDashboardModel {
 
     // Join using a single column or Seq if multiple columns
     val userOrgHierarchyDataDF = userOrgDF.join(broadcast(orgHierarchyData), Seq("userOrgID"), "left")
-    val oldAssessmentData = oldAssessmentDetailsDataframe()
-    val fullReportDFOld = oldAssessmentData.join(allCourseProgramDetailsDF, Seq("courseID"), "left").select(
-      oldAssessmentData.columns.map(col) ++
-        Seq(col("courseName"), col("courseOrgID")): _*)
+    val oldAssessmentData = cache.load("oldAssessmentDetails").withColumnRenamed("user_id", "userID").withColumnRenamed("parent_source_id", "courseID")
+    val fullReportDFOld = oldAssessmentData.join(allCourseProgramDetailsDF, Seq("courseID"), "left")
       .join(userOrgHierarchyDataDF, Seq("userID"), "left")
-      .withColumn("Assessment Type", lit("Learning Resource"))
-      .withColumn("Total Questions", col("correct_count") + col("incorrect_count") + col("not_answered_count"))
-      .withColumn("Assessment Publish Date", lit(null).cast("date"))
-      .withColumn("Assessment Duration", lit(null).cast("string"))
-      .withColumn("Last Attempted Date", lit(null).cast("date"))
-      .withColumn("No. of Retakes", lit(null).cast("integer"))
+      .withColumn("assessment_type", lit("Learning Resource"))
+      .withColumn("total_questions", col("correct_count") + col("incorrect_count") + col("not_answered_count"))
+      .withColumn("assessment_publish_date", lit(null).cast("date"))
+      .withColumn("assessment_duration", lit(null).cast("string"))
+      .withColumn("last_attempted_date", lit(null).cast("date"))
+      .withColumn("retakes", lit(null).cast("integer"))
       .withColumn("assessEndTime", lit(null).cast("bigint"))
       .withColumn("assessChildID", lit(null).cast("string"))
       .withColumn("Pass", when(col("result_percent") >= col("pass_percent"), lit("Yes")).otherwise(lit("No")))
       .withColumn("Tags", concat_ws(", ", col("additionalProperties.tag")))
       .select(
-        col("userID").alias("User ID"),
-        col("source_id").alias("assessID"),
+        col("userID"),
+        col("source_id").alias("assessment_id"),
         col("courseOrgID"),
         col("assessChildID"),
         col("userOrgID"),
-        col("fullName").alias("Full Name"),
+        col("fullName"),
         col("professionalDetails.designation").alias("Designation"),
         col("personalDetails.primaryEmail").alias("E mail"),
         col("personalDetails.mobile").alias("Phone Number"),
@@ -137,34 +136,34 @@ object CourseBasedAssessmentModel extends AbsDashboardModel {
         col("ministry_name").alias("Ministry"),
         col("dept_name").alias("Department"),
         col("userOrgName").alias("Organisation"),
-        col("source_title").alias("Assessment Name"),
-        col("Assessment Type"),
-        col("courseOrgID").alias("Assessment/Content Provider"),
-        col("Assessment Publish Date"),
-        col("courseName").alias("Course Name"),
-        col("courseID").alias("Course ID"),
-        col("Assessment Duration"),
-        col("Last Attempted Date"),
-        col("result_percent").alias("Latest Percentage Achieved"),
-        col("pass_percent").alias("Cut off Percentage"),
+        col("source_title").alias("assessment_name"),
+        col("assessment_type"),
+        col("courseOrgID").alias("assessment_content_provider"),
+        col("assessment_publish_date"),
+        col("courseName").alias("course_name"),
+        col("courseID").alias("course_id"),
+        col("assessment_duration"),
+        col("last_attempted_date"),
+        col("result_percent").alias("latest_percentage_achieved"),
+        col("pass_percent").alias("assessPercentage"),
         col("Pass"),
-        col("Total Questions"),
-        col("incorrect_count").alias("No.of Incorrect Responses"),
-        col("not_answered_count").alias("Unattempted Questions"),
-        col("`No. of Retakes`"),
+        col("total_questions"),
+        col("incorrect_count"),
+        col("not_answered_count").alias("unattempted_questions"),
+        col("retakes"),
         col("assessEndTime"),
         col("userOrgID").alias("mdoid")
-      ).repartition(1)
+      )
 
     val fullReportDF = fullReportDFNew.union(fullReportDFOld)
-      .withColumn("Report_Last_Generated_On", currentDateTime)
+      .withColumn("Report_Last_Generated_On", currentDateTime).dropDuplicates("userID", "assessment_id", "course_id")
     val reportPath = s"${conf.cbaReportPath}/${today}"
     // generateReport(fullReportDF, s"${reportPath}-full")
 
     val mdoReportDF = fullReportDF
       .select(
-        col("User ID"),
-        col("Full Name"),
+        col("userID").alias("User ID"),
+        col("fullName").alias("Full Name"),
         col("Designation"),
         col("E mail"),
         col("Phone Number"),
@@ -173,21 +172,21 @@ object CourseBasedAssessmentModel extends AbsDashboardModel {
         col("Ministry"),
         col("Department"),
         col("Organisation"),
-        col("Assessment Name"),
-        col("Assessment Type"),
-        col("Assessment/Content Provider"),
-        col("Assessment Publish Date"),
-        col("Course Name"),
-        col("Course ID"),
-        col("Assessment Duration"),
-        col("Last Attempted Date"),
-        col("Latest Percentage Achieved"),
-        col("Cut off Percentage"),
+        col("assessment_name").alias("Assessment Name"),
+        col("assessment_type").alias("Assessment Type"),
+        col("assessment_content_provider").alias("Assessment/Content Provider"),
+        col("assessment_publish_date").alias("Assessment Publish Date"),
+        col("course_name").alias("Course Name"),
+        col("course_id").alias("Course ID"),
+        col("assessment_duration").alias("Assessment Duration"),
+        col("last_attempted_date").alias("Last Attempted Date"),
+        col("latest_percentage_achieved").alias("Latest Percentage Achieved"),
+        col("assessPercentage").alias("Cut off Percentage"),
         col("Pass"),
-        col("Total Questions"),
-        col("No.of Incorrect Responses"),
-        col("Unattempted Questions"),
-        col("No. of Retakes"),
+        col("total_questions").alias("Total Questions"),
+        col("incorrect_count").alias("No.of Incorrect Responses"),
+        col("unattempted_questions").alias("Unattempted Questions"),
+        col("retakes").alias("No. of Retakes"),
         col("mdoid"),
         col("Report_Last_Generated_On")
       ).coalesce(1)
@@ -197,24 +196,24 @@ object CourseBasedAssessmentModel extends AbsDashboardModel {
       syncReports(s"${conf.localReportDir}/${reportPath}", reportPath)
     }
 
-    val warehouseDF = finalDF
+    val warehouseDF = fullReportDF
       .withColumn("data_last_generated_on", currentDateTime)
       .select(
-        col("User ID").alias("user_id"),
-        col("Course ID").alias("content_id"),
-        col("assessChildID").alias("assessment_id"),
-        col("Assessment Name").alias("assessment_name"),
-        col("Assessment Type").alias("assessment_type"),
-        col("Assessment Duration").alias("assessment_duration"),
-        col("Assessment Duration").alias("time_spent_by_the_user"),
+        col("userID").alias("user_id"),
+        col("course_id").alias("content_id"),
+        col("assessment_id"),
+        col("assessment_name").alias("assessment_name"),
+        col("assessment_type").alias("assessment_type"),
+        col("assessment_duration").alias("assessment_duration"),
+        col("assessment_duration").alias("time_spent_by_the_user"),
         //from_unixtime(col("assessEndTime"), "dd/MM/yyyy").alias("completion_date"),
-        date_format(from_unixtime(col("assessEndTime")), "yyyy-MM-dd HH:mm:ss").alias("completion_date"),
-        col("Latest Percentage Achieved").alias("score_achieved"),
-        col("Total Questions").alias("overall_score"),
-        col("Cut off Percentage").alias("cut_off_percentage"),
-        col("Total Questions").alias("total_question"),
-        col("No.of Incorrect Responses").alias("number_of_incorrect_responses"),
-        col("No. of Retakes").alias("number_of_retakes"),
+        date_format(from_unixtime(col("assessEndTime")), dateTimeFormat).alias("completion_date"),
+        col("latest_percentage_achieved").alias("score_achieved"),
+        col("total_questions").alias("overall_score"),
+        col("assessPercentage").alias("cut_off_percentage"),
+        col("total_questions").alias("total_question"),
+        col("incorrect_count").alias("number_of_incorrect_responses"),
+        col("retakes").alias("number_of_retakes"),
         col("data_last_generated_on")
       )
     generateReport(warehouseDF.coalesce(1), s"${reportPath}-warehouse")
