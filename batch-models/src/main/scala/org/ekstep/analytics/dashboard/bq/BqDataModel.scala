ef0c945cbddea7fa16dfa41f09d66bdb0105631b
Shishir Suman <shishir.suman@tarento.com>
2024-10-14 20:32:16 +0530

    changes for updating avro folder name

--- batch-models/src/main/scala/org/ekstep/analytics/dashboard/bq/BqDataModel.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/dashboard/bq/BqDataModel.scala
@@ -9,9 +9,6 @@ import sys.process._
 
 object BqDataModel extends AbsDashboardModel {
   override def processData(timestamp: Long)(implicit spark: SparkSession, sc: SparkContext, fc: FrameworkContext, conf: DashboardConfig): Unit = {
-    // today's date
-    val today = getDate()
-
     // root path to bq scripts
     val bqScriptPath = conf.bqScriptPath
 
