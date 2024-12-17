eba503652fa25ea986ca2abe36e352a0a96a9ae9
Shishir Suman <shishir.suman@tarento.com>
2024-12-17 12:58:22 +0530

    changes for adding new column (content_sub_type) for containing course category details

--- batch-models/src/main/scala/org/ekstep/analytics/dashboard/DataUtil.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/dashboard/DataUtil.scala
@@ -644,7 +644,8 @@ object DataUtil extends Serializable {
         col("competencies_v5.competencyAreaId"),
         col("competencies_v5.competencyThemeId"),
         col("competencies_v5.competencySubThemeId"),
-        col("contentLanguage")
+        col("contentLanguage"),
+        col("courseCategory").alias("content_sub_type")
       ).dropDuplicates("courseID", "category")
       .na.fill(0.0, Seq("courseDuration"))
       .na.fill(0, Seq("courseResourceCount"))
