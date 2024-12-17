e83cbea784d162673fe81deaa3d174070397f445
Shishir Suman <shishir.suman@tarento.com>
2024-12-17 13:01:30 +0530

    changes for updating warehouse-schema.sql

--- batch-models/src/main/resources/warehouse-schema.sql
+++ batch-models/src/main/resources/warehouse-schema.sql
@@ -242,5 +242,8 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON user_enrolments TO postgres;
 ALTER TABLE user_detail RENAME COLUMN is_verified_karmayogi TO profile_status;
 ALTER TABLE user_detail ALTER COLUMN profile_status TYPE character varying(15);
 
-alter table user_enrolments drop column marked_as_not_my_user;
+alter table user_detail drop column marked_as_not_my_user;
 alter table user_detail rename column is_verified_karmayogi to profile_status;
+
+// adding new column in content
+ALTER TABLE content ADD COLUMN content_sub_type character varying(50);
