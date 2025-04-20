355d747fc5148711e0a249cd282eb02986e07429
Shishir Suman <shishir.suman@tarento.com>
2025-04-20 22:09:41 +0530

    changes for fixing merge error

--- batch-models/src/main/scala/org/ekstep/analytics/dashboard/exhaust/DataExhaustModel.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/dashboard/exhaust/DataExhaustModel.scala
@@ -1,14 +1,19 @@
 package org.ekstep.analytics.dashboard.exhaust
 
+import org.apache.spark.SparkContext
 import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 import org.ekstep.analytics.dashboard.DashboardUtil._
 import org.ekstep.analytics.dashboard.DataUtil._
 import org.apache.spark.sql.types._
+
 import scala.util._
 import scala.concurrent._
 import ExecutionContext.Implicits.global
 import org.ekstep.analytics.dashboard.{AbsDashboardModel, DashboardConfig}
 import org.ekstep.analytics.framework._
+
+import scala.concurrent.duration.DurationInt
 
 /**
  * Model for processing dashboard data
