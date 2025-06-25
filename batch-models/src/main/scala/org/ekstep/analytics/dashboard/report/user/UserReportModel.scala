0ee52831e5eb3afd54c3654f7d3b6cd1dc9e5bc8
Abhishek P N <abhishekpn98@gmail.com>
2025-06-25 15:50:39 +0530

    #KB-9681: Added pincode to user report warehouse for report generation service

--- batch-models/src/main/scala/org/ekstep/analytics/dashboard/report/user/UserReportModel.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/dashboard/report/user/UserReportModel.scala
@@ -125,7 +125,7 @@ object UserReportModel extends AbsDashboardModel {
         col("professionalDetails.designation").alias("designation"),
         col("personalDetails.primaryEmail").alias("email"),
         col("personalDetails.mobile").alias("phone_number"),
-        col("personalDetails.pincode").alias("Pincode"),
+        col("personalDetails.pincode").alias("pincode"),
         col("professionalDetails.group").alias("groups"),
         col("Tag").alias("tag"),
         col("userProfileStatus").alias("profile_status"),
