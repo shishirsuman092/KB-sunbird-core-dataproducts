375ac63855805d45bf51efc57875321907455aeb
Varsha Mahuli <varsha.mahuli@tarento.com>
2025-07-09 20:58:03 +0530

    added missing imports

--- batch-models/src/main/scala/org/ekstep/analytics/dashboard/nationallearningweek/NationalLearningWeekModel.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/dashboard/nationallearningweek/NationalLearningWeekModel.scala
@@ -3,6 +3,7 @@ package org.ekstep.analytics.dashboard.nationallearningweek
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.Column
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{StringType, IntegerType, LongType, BooleanType, StructType}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.expressions.Window
 import org.ekstep.analytics.dashboard.DataUtil._
